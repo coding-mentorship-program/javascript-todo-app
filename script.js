c7a77fbfd6c2e13be42e0e6607551e33cf18dd3a
jonathans199c <jonathasa@city-furniture.com>
2023-07-12 15:37:31 -0400

    updated with localStorage for user personalized todos

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let input = document.getElementById('input_field')
 let todos = document.getElementById('allTodos')
 
-let allTodos = []
+let allTodos = JSON.parse(localStorage.getItem('user_todos')) ? JSON.parse(localStorage.getItem('user_todos')) : []
 
 input.addEventListener('keydown', event => {
   if (event.keyCode === 13) addTodo()
 })
 
-function deleteOne(id) {
-  allTodos.splice(id, 1)
-  displayAllItems()
+function updateLocalStorage() {
+  localStorage.setItem('user_todos', JSON.stringify(allTodos))
 }
 
 function displayAllItems() {
@@ -19,7 +18,7 @@ function displayAllItems() {
     const newLi = document.createElement('div') // create a new div for each item in array
 
     // add innerHTML to the new div created above
-    newLi.innerHTML = `<div class="todo_item"><h1 onclick="done(${index})"  style="text-decoration: ${
+    newLi.innerHTML = `<div class="todo_item"><h1 onclick="done(${item.id})"  style="text-decoration: ${
       item.done ? 'line-through' : ''
     }">${item.todo} </h1>
     <button onclick="deleteOne(${index})">x</button>
@@ -29,12 +28,22 @@ function displayAllItems() {
   })
 }
 
+displayAllItems()
+
+function deleteOne(index) {
+  allTodos.splice(index, 1)
+  updateLocalStorage()
+  displayAllItems()
+}
+
 function addTodo() {
   // check if the input field is filled in
   if (input.value !== '') {
     // if filled in push brand new object into the array
     allTodos.push({ todo: input.value, done: false, id: allTodos.length + 1 })
     input.value = '' // reset the input value to empty
+
+    updateLocalStorage()
 
     displayAllItems() // fire this function to display the items
   } else {
@@ -43,18 +52,19 @@ function addTodo() {
 }
 
 function done(id) {
-  // find the item that user clicked on
-  const itemFound = allTodos.find(x => x.id === id + 1)
-
-  // update the done attribute to true or false
-  itemFound.done = itemFound.done ? false : true
-
   // find item in array, remove it and replace with new itemFound object
-  allTodos.splice(id, 1, itemFound)
+  for (const i of allTodos) {
+    // update the done attribute to true or false
+    if (i.id === id) i.done ? (i.done = false) : (i.done = true)
+  }
+
+  // update local storage with updated array of items
+  updateLocalStorage()
   displayAllItems()
 }
 
 function clearAll() {
   allTodos = []
+  localStorage.clear()
   displayAllItems()
 }
